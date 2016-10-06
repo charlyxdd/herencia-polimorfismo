794e5539092192e7cede5ae5c825360c180db6e9
Geisler Sanchez <Geisler Sanchez>
2016-10-05 19:28:25 -0500

    Clase animal

--- src/main/principal.java
+++ src/main/principal.java
@@ -2,6 +2,6 @@ package main;
 
 public class principal {
 	public static void main(String args[]){
-		
+
 	}
 }
--- src/objetos/Animal.java
+++ src/objetos/Animal.java
@@ -1,5 +1,31 @@
 package objetos;
 
-public class Animal {
+public class Animal{
+	private String nombre;
+	private String especie;
+	private int edad;
+
+	public Animal(String nombre, String especie, int edad){
+		this.nombre=nombre;
+		this.especie=especie;
+		this.edad=edad;
+	}
+
+	public Animal(){
+
+	}
+
+	public Animal(String nombre){
+
+	}
+
+
+	public setNombre(String nombre){
+		this.nombre=nombre;
+	}
+
+	public getNombre(){
+		return nombre;
+	}
 
 }
